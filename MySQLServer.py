fd3d8c63ecf72fb96a7d0373b28e421fb9758ba6
kothnyagol@gmail.com <kothnyagol@gmail.com>
2025-12-14 10:47:56 -0800

    Build A Database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# MySQLServer.py
+
 
 import mysql.connector
-from mysql.connector import Error
 
 
 def create_database():
@@ -20,7 +19,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
